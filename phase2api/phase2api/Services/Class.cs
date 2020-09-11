3752fbdaa963361fa2390aa3c89115e7fa350aa5
Lance Delos Reyes <lan_drey19@hotmail.com>
2020-09-11 21:11:50 +1200

    added services

---
+++ phase2api/phase2api/Services/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phase2api.Services
+{
+    public class Class
+    {
+    }
+}
---
+++ phase2api/phase2api/Services/Interface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phase2api.Services
+{
+    public interface IEntriesServices
+    {
+        public List<Entry> GetEntries();
+
+        public Entry AddEntry(Entry theirEntry);
+
+        public Entry UpdateEntry(string id, Entry theirEntry);
+
+        public string DeleteEntry(string id);
+    }
+}
