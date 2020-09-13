99000e88e9dc299515fa5d549e7ec1c4f7bb99a1
Lance Delos Reyes <lan_drey19@hotmail.com>
2020-09-13 18:38:40 +1200

    created context

---
+++ AislesApi/AislesApi/Models/AisleContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AislesAPI.Models
+{
+    public class AisleContext : DbContext
+    {
+
+        public AisleContext(DbContextOptions<AisleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aisle> Aisles { get; set; }
+        public DbSet<AisleSection> AisleSections { get; set; }
+    }
+}
